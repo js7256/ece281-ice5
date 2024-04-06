51d264f9e48477ca10900610892fcd808b268eb8
js7256 <113559860+js7256@users.noreply.github.com>
2024-04-05 22:03:26 -0600

    update

--- src/hdl/elevator_controller_fsm.vhd
+++ src/hdl/elevator_controller_fsm.vhd
@@ -90,26 +90,27 @@ architecture Behavioral of elevator_controller_fsm is
 	type sm_floor is (s_floor1, s_floor2, s_floor3, s_floor4);
 	
 	-- Here you create variables that can take on the values defined above. Neat!	
-	signal f_Q, f_Q_next: sm_floor;
+	signal f_Q, f_Q_next : sm_floor;
 
 begin
 
 	-- CONCURRENT STATEMENTS ------------------------------------------------------------------------------
 	-- Next State Logic
-    f_Q_next <= <state> when (<condition>) else -- going up
-                ...
-                ...
-                ... -- going down
-                ...
-                ... else
-                ...; -- default case
+    f_Q_next <= -- going up
+                s_floor2 when i_up_down = '1' and f_Q = s_floor1 else
+                s_floor3 when i_up_down = '1' and f_Q = s_floor2 else
+                s_floor4 when i_up_down = '1' and f_Q = s_floor3 else
+                s_floor2 when i_up_down = '0' and f_Q = s_floor3 else
+                s_floor3 when i_up_down = '0' and f_Q = s_floor4 else -- going down
+                s_floor1; -- default case
   
 	-- Output logic
     with f_Q select
-        o_floor <= <value> when s_floor1,
-                ...
-                ...
-                <value> when others; -- default is floor 2
+        o_floor <= 
+                   "0001" when s_floor1,
+                   "0011" when s_floor3,
+                   "0100" when s_floor4,
+                   "0010" when others; -- default is floor 2
 	
 	-------------------------------------------------------------------------------------------------------
 	
@@ -118,7 +119,14 @@ begin
 	register_proc : process (i_clk)
     begin
          -- synchronous reset
-        
+        if i_reset = '1' then
+            f_Q <= s_floor2;
+        elsif rising_edge(i_clk) then
+            f_Q <= f_Q_next;
+        end if;
+        if i_stop = '1' then 
+            f_Q <= f_Q;
+        end if;
         -- if elevator is enabled, advance floors
         -- if not enabled, stay at current floor
     
--- src/hdl/elevator_controller_fsm_tb.vhd
+++ src/hdl/elevator_controller_fsm_tb.vhd
@@ -64,9 +64,12 @@ architecture test_bench of elevator_controller_fsm_tb is
 			   o_floor 	 : out STD_LOGIC_VECTOR (3 downto 0));
 	end component elevator_controller_fsm;
 	
+	type sm_floor is (s_floor1, s_floor2, s_floor3, s_floor4); 
+	
 	-- test signals
 	signal w_clk, w_reset, w_stop, w_up_down : std_logic := '0';
 	signal w_floor : std_logic_vector(3 downto 0) := (others => '0');
+	signal f_Q, f_Q_next : sm_floor;
   
 	-- 50 MHz clock
 	constant k_clk_period : time := 20 ns;
@@ -103,20 +106,43 @@ begin
         w_reset <= '1';  wait for k_clk_period;
             assert w_floor = "0010" report "bad reset" severity failure; 
         -- clear reset
+        w_reset <= '0'; 
 		
 		-- active UP signal
-		w_up_down <= '1'; 
+		w_up_down <= '0'; wait for k_clk_period;
+		w_up_down <= '1'; --setting it to floor 1 for easy-to-read progression
 		
 		-- stay on each o_floor for 2 cycles and then move up to the next o_floor
+        w_stop <= '1';  wait for k_clk_period * 2;
+            assert w_floor = "0001" report "bad wait on floor1" severity failure;
+        w_stop <= '0';  wait for k_clk_period;
+            assert w_floor = "0010" report "bad up from floor1" severity failure;		
         w_stop <= '1';  wait for k_clk_period * 2;
             assert w_floor = "0010" report "bad wait on floor2" severity failure;
         w_stop <= '0';  wait for k_clk_period;
             assert w_floor = "0011" report "bad up from floor2" severity failure;
+        w_stop <= '1';  wait for k_clk_period * 2;
+            assert w_floor = "0011" report "bad wait on floor3" severity failure;
+        w_stop <= '0';  wait for k_clk_period;
+            assert w_floor = "0100" report "bad up from floor3" severity failure;
+        w_stop <= '1';  wait for k_clk_period * 2;
+            assert w_floor = "0100" report "bad wait on floor4" severity failure;
 		-- rest of cases
         
         -- go back DOWN
-          
-		  	
+        w_up_down <= '0';
+        w_stop <= '0';  wait for k_clk_period;
+            assert w_floor = "0011" report "bad down from floor4" severity failure;
+        w_stop <= '1';  wait for k_clk_period * 2;
+            assert w_floor = "0011" report "bad wait on floor3" severity failure;        
+        w_stop <= '0';  wait for k_clk_period;
+            assert w_floor = "0010" report "bad down from floor3" severity failure;
+        w_stop <= '1';  wait for k_clk_period * 2;
+            assert w_floor = "0010" report "bad wait on floor2" severity failure;		
+        w_stop <= '0';  wait for k_clk_period;
+            assert w_floor = "0001" report "bad down from floor2" severity failure;
+        w_stop <= '1';  wait for k_clk_period * 2;
+            assert w_floor = "0001" report "bad wait on floor1" severity failure;  	
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
